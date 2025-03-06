b1faff48dfecfd467f89d0b9eb5650159c3de0cf
Arthurecomp <ambs@poli.br>
2025-03-06 11:05:34 -0300

    Initial commit

---
+++ src/main/java/me/dio/avanade_2024/Avanade2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.avanade_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Avanade2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Avanade2024Application.class, args);
+	}
+
+}
